fd023a93927f06bf5a398a4b224eac9236bf75b1
Louay Rougui <louayrougui101@gmail.com>
2026-07-07 14:39:10 +0100

    added updateTeam,kickMember to team controller

--- backend/src/team/team.controller.ts
+++ backend/src/team/team.controller.ts
@@ -11,11 +11,13 @@ import {
   UseGuards,
   HttpCode,
   HttpStatus,
+  Patch,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guards/jwt.guard';
 import type { Request } from 'express';
 import { CreateTeamDto } from './dto/createTeam.dto';
 import { JoinTeamDto } from './dto/joinTeam.dto';
+import { UpdateTeamDto } from './dto/updateTeam.dto';
 
 @UseGuards(JwtGuard)
 @Controller('events/:eventId/teams')
@@ -31,6 +33,15 @@ export class TeamController {
   getTeamDetails(@Param('eventId') eventId: string, @Req() req: Request) {
     const userId = (req.user as any).id;
     return this.teamService.getTeamDetails(eventId, userId);
+  }
+  @Get(':teamId')
+  getTeamById(
+    @Param('eventId') eventId: string,
+    @Param('teamId') teamId: string,
+    @Req() req: Request,
+  ) {
+    const userId = (req.user as any).id;
+    return this.teamService.getTeamById(eventId, teamId, userId);
   }
 
   @Post()
@@ -56,17 +67,38 @@ export class TeamController {
     return this.teamService.joinTeam(eventId, teamId, dto.password, userId);
   }
 
+  @Patch(':teamId')
+  @HttpCode(HttpStatus.OK)
+  updateTeam(
+    @Param('teamId') teamId: string,
+    @Body() dto: UpdateTeamDto,
+    @Req() req: Request,
+  ) {
+    const captainId = (req.user as any).id;
+    return this.teamService.updateTeam(teamId, captainId, dto);
+  }
   @Delete(':teamId/leave')
   @HttpCode(HttpStatus.OK)
   leaveTeam(@Param('teamId') teamId: string, @Req() req: Request) {
-    const userId = (req.user as any).id;
-    return this.teamService.leaveTeam(teamId, userId);
+    const captainId = (req.user as any).id;
+    return this.teamService.leaveTeam(teamId, captainId);
   }
 
   @Delete(':teamId')
   @HttpCode(HttpStatus.OK)
   deleteTeam(@Param('teamId') teamId: string, @Req() req: Request) {
-    const userId = (req.user as any).id;
-    return this.teamService.deleteTeam(teamId, userId);
+    const captainId = (req.user as any).id;
+    return this.teamService.deleteTeam(teamId, captainId);
+  }
+
+  @Delete(':teamId/members/:userId')
+  @HttpCode(HttpStatus.OK)
+  kickMember(
+    @Param('teamId') teamId: string,
+    @Param('userId') targetUserId: string,
+    @Req() req: Request,
+  ) {
+    const captainId = (req.user as any).id;
+    return this.teamService.kickMember(teamId, captainId, targetUserId);
   }
 }
